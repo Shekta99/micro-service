d2c561051da8213b2990898339172926df0c5997
Sergio Marentes <sergiomarentes705@gmail.com>
2022-02-22 08:54:49 -0500

    rollback dogo function

--- functions/dogos.ts
+++ functions/dogos.ts
@@ -1,24 +1,10 @@
 import { Handler } from "@netlify/functions";
-import fetch from "node-fetch";
 
 const handler: Handler = async (event, context) => {
-  const response = await fetch("https://random.dog/doggos");
-  const data = await response.json();
-
-  if (Array.isArray(data)) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        dogs: {
-          images: data.slice(0, 10).map((url) => `https://random.dog/${url}`),
-        },
-      }),
-    };
-  } else {
-    return {
-      statusCode: 404,
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello Dogo World" }),
+  };
 };
 
 export { handler };
