34c2371429a8f9c7a4be3787291f51ec8f612a1f
Sergio Marentes <sergiomarentes705@gmail.com>
2022-02-22 04:55:40 -0500

    adding third api

--- functions/dogos.ts
+++ functions/dogos.ts
@@ -1,10 +1,24 @@
 import { Handler } from "@netlify/functions";
+import fetch from "node-fetch";
 
 const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello Dogo World" }),
-  };
+  const response = await fetch("https://random.dog/doggos");
+  const data = await response.json();
+
+  if (Array.isArray(data)) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        dogs: {
+          images: data.slice(0, 10).map((url) => `https://random.dog/${url}`),
+        },
+      }),
+    };
+  } else {
+    return {
+      statusCode: 404,
+    };
+  }
 };
 
 export { handler };
